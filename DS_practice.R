0cf0662f65620d34f259aa0e86786f39a687f9a9
Aguibou Ndiaye <aguiboundiaye@gmail.com>
2024-04-01 14:54:44 +0200

    test

---
+++ DS_practice.R
@@ -0,0 +1,9 @@
+install.packages("tidyverse")
+installed.packages("dslab")
+installed.packages("ggplot")
+
+library(tidyverse)
+library(dslabs)
+library(ggplot2)
+                 
+                 
